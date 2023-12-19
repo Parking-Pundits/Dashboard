bcf970e6a09fb6072a5fa638b1c543f9cca40a7c
Radhika Bhoyar <bhoyarradhika17@gmail.com>
2023-12-19 22:59:56 +0530

    JalRakshak

--- src/pages/Pilferage.jsx
+++ src/pages/Pilferage.jsx
@@ -1,121 +1,64 @@
-// import { ColumnDirective, ColumnsDirective, KanbanComponent } from '@syncfusion/ej2-react-kanban';
-// import React from 'react';
-
-// import { Header } from '../components';
-// import { kanbanData, kanbanGrid } from '../data/dummy';
-
-// const Pilferage = () => (
-//   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-//     <Header category="App" title="Kanban" />
-//     <KanbanComponent
-//       id="kanban"
-//       keyField="Status"
-//       dataSource={kanbanData}
-//       cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
-//     >
-//       <ColumnsDirective>
-//         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-//         {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-//       </ColumnsDirective>
-//     </KanbanComponent>
-//   </div>
-// );
-
-// export default Pilferage;
-
-
-import { get, ref, set } from 'firebase/database';
-import Papa from 'papaparse';
-import React, { useState } from 'react';
-import { database } from '../firebaseConfig';
+import { useEffect, useState } from 'react';
 
 const Pilferage = () => {
-  const [file, setFile] = useState(null);
-  const [apiResponse, setApiResponse] = useState(null);
+  const [sensorNumbers, setSensorNumbers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleFileChange = (e) => {
-    const uploadedFile = e.target.files[0];
-    setFile(uploadedFile);
-  };
-
-  const handleUpload = async () => {
-    if (file) {
+  useEffect(() => {
+    const fetchData = async () => {
       setIsLoading(true);
-
-      const formData = new FormData();
-      formData.append('file', file);
-
-      const reader = new FileReader();
-      reader.onload = async (event) => {
-        const csvData = event.target.result;
-        const parsedData = Papa.parse(csvData, { header: true }).data;
-
-        try {
-          const response = await fetch('http://127.0.0.1:5000/pilferage', {
-            method: 'POST',
-            body: JSON.stringify(parsedData),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-        
-          if (response.ok) {
-            const responseData = await response.json();
-            setApiResponse(responseData);
-            const frudsRef = ref(database, 'frudsDetects');
-            const frudsSnapshot = await get(frudsRef);
-            const currentCount = frudsSnapshot.val() || 0;
-        
-            // Update Firebase with the incremented count
-            set(frudsRef, currentCount + pilferageCount);
-        
-          } else {
-            console.error('Failed to fetch data');
-            
-            // Retrieve current count from Firebase
-              }
-        } catch (error) {
-          console.error('Error:', error);
+      try {
+        const response = await fetch('http://127.0.0.1:5000/auto_predictions');
+        if (response.ok) {
+          const responseData = await response.json();
+          setSensorNumbers(responseData);
+        } else {
+          console.error('Failed to fetch data');
         }
-        
+      } catch (error) {
+        console.error('Error:', error);
+      }
+      setIsLoading(false);
+    };
 
-        setIsLoading(false);
-      };
+    fetchData();
+  }, []);
 
-      reader.readAsText(file);
-    } else {
-      console.error('No file selected');
+  const renderTable = () => {
+    if (isLoading) {
+      return <p>Loading...</p>;
     }
-  };
 
-  const renderColumns = () => {
-    if (apiResponse) {
-      return Object.keys(apiResponse).map((key) => (
-        <div className="column" key={key}>
-          <h4>{key}</h4>
-          <pre>{JSON.stringify(apiResponse[key], null, 2)}</pre>
-        </div>
-      ));
+    if (!sensorNumbers || !sensorNumbers.length) {
+      return <p>No data available.</p>;
     }
-    return null;
+
+    return (
+      <div style={{ margin: '20px 0' }}>
+        <h1>These Sensors may be Pilfreaged </h1>
+        <br /><br />
+        <table style={{ borderCollapse: 'collapse', width: '100%', border: '1px solid grey' }}>
+          <thead>
+            <tr style={{ background: 'lightgrey' }}>
+              <th style={{ border: '1px solid grey', padding: '8px' }}>Sensor Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sensorNumbers.map((sensor, index) => (
+              <tr key={index}>
+                <td style={{ border: '1px solid grey', padding: '8px' }}>{sensor}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   };
 
   return (
     <div className="container">
-      <div className="upload-section">
-        <input type="file" accept=".csv" onChange={handleFileChange} />
-        <button onClick={handleUpload} disabled={isLoading}>
-          {isLoading ? 'Uploading...' : 'Upload CSV'}
-        </button>
-      </div>
-
-      <div className="response-section">
-        <h3>API Response:</h3>
-        <div className="columns-container" style={{ display: 'flex' }}>
-          {renderColumns()}
-        </div>
-      </div>
+      <h3>API Response:</h3>
+      <div className="data-container">{renderTable()}</div>
     </div>
   );
 };
--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -1,83 +1,28 @@
-import Papa from 'papaparse';
-import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 const Demo = () => {
-  const [file, setFile] = useState(null);
-  const [apiResponse, setApiResponse] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    // Fetch the embed configuration for your report (replace with your own API call)
+    const fetchEmbedConfig = async () => {
+      try {
+        // Make API call to get the embed configuration
+        const embedConfig = await fetch('your-api-endpoint-to-get-embed-config');
+        
+        // Use the retrieved configuration to embed the report
+        const embedContainer = document.getElementById('reportContainer');
+        window.powerbi.embed(embedContainer, embedConfig);
+      } catch (error) {
+        console.error('Failed to fetch embed configuration:', error);
+      }
+    };
 
-  const handleFileChange = (e) => {
-    const uploadedFile = e.target.files[0];
-    setFile(uploadedFile);
-  };
-
-  const handleUpload = async () => {
-    if (file) {
-      setIsLoading(true);
-
-      const formData = new FormData();
-      formData.append('file', file);
-
-      const reader = new FileReader();
-      reader.onload = async (event) => {
-        const csvData = event.target.result;
-        const parsedData = Papa.parse(csvData, { header: true }).data;
-
-        try {
-          const response = await fetch('http://127.0.0.1:5000/pilferage', {
-            method: 'POST',
-            body: JSON.stringify(parsedData),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-
-          if (response.ok) {
-            const responseData = await response.json();
-            setApiResponse(responseData);
-          } else {
-            console.error('Failed to fetch data');
-          }
-        } catch (error) {
-          console.error('Error:', error);
-        }
-
-        setIsLoading(false);
-      };
-
-      reader.readAsText(file);
-    } else {
-      console.error('No file selected');
-    }
-  };
-
-  const renderColumns = () => {
-    if (apiResponse) {
-      return Object.keys(apiResponse).map((key) => (
-        <div className="column" key={key}>
-          <h4>{key}</h4>
-          <pre>{JSON.stringify(apiResponse[key], null, 2)}</pre>
-        </div>
-      ));
-    }
-    return null;
-  };
+    fetchEmbedConfig();
+  }, []);
 
   return (
-    <div className="container">
-      <div className="upload-section">
-        <input type="file" accept=".csv" onChange={handleFileChange} />
-        <button onClick={handleUpload} disabled={isLoading}>
-          {isLoading ? 'Uploading...' : 'Upload CSV'}
-        </button>
-      </div>
-
-      <div className="response-section">
-        <h3>API Response:</h3>
-        <div className="columns-container" style={{ display: 'flex' }}>
-          {renderColumns()}
-        </div>
-      </div>
+    <div>
+      {/* Container to hold the embedded report */}
+      <div id="reportContainer" style={{ height: '600px', width: '800px' }}></div>
     </div>
   );
 };
